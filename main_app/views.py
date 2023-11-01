86a1ad7eed48e0d2aee841f6dd3e5d2d19ede218
Johny270 <jeantakam56@yahoo.com>
2023-11-01 16:39:41 -0300

    Complete detail functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
-class Book:
-  def __init__(self, title, author, description, year):
-    self.title = title
-    self.author = author
-    self.description = description
-    self.year = year
-
-books = [
-  Book('Think and Grow Rich', 'Napoleon Hill', 'A book about success', '1900'),
-  Book('Think and Grow Rich', 'Napoleon Hill', 'A book about success', '1900'),
-  Book('Think and Grow Rich', 'Napoleon Hill', 'A book about success', '1900')
-]
 
 # Create your views here.
 def home(request):
@@ -22,4 +11,9 @@ def about(request):
   return render(request, 'about.html')
 
 def book_index(request):
+  books = Book.objects.all()
   return render(request, 'books/index.html', { 'books': books })
+
+def book_detail(request, book_id):
+  book = Book.objects.get(id=book_id)
+  return render(request, 'books/detail.html', { 'book': book})
