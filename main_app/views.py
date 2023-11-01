10f15957313fcb31a8b70d7b98f816c1abccc380
Johny270 <jeantakam56@yahoo.com>
2023-11-01 09:57:40 -0300

    Stub up home and index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+class Book:
+  def __init__(self, title, author, description, year):
+    self.title = title
+    self.author = author
+    self.description = description
+    self.year = year
+
+books = [
+  Book('Think and Grow Rich', 'Napoleon Hill', 'A book about success', '1900'),
+  Book('Think and Grow Rich', 'Napoleon Hill', 'A book about success', '1900'),
+  Book('Think and Grow Rich', 'Napoleon Hill', 'A book about success', '1900')
+]
 
 # Create your views here.
+def home(request):
+  return HttpResponse('<h1>Hellow World</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def book_index(request):
+  return render(request, 'books/index.html', { 'books': books })
