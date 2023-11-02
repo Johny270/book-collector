c71f2ece6f5eca9eea08ef3c07e13de77aac151f
Johny270 <jeantakam56@yahoo.com>
2023-11-02 10:51:36 -0300

    Implement login and signup functionalities

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('books/', views.book_index, name='book-index'),
     path('books/<int:book_id>', views.book_detail, name='book-detail'),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('books/<int:pk>/delete', views.BookDelete.as_view(), name='book-delete'),
     path('books/<int:book_id>/add-review/', views.add_review, name='add-review'),
     path('books/<int:book_id>/assoc-cover/<int:cover_id>/', views.assoc_cover, name='assoc-cover'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('covers/create/', views.CoverCreate.as_view(), name='cover-create'),
     path('covers/<int:pk>/', views.CoverDetail.as_view(), name ='cover-detail'),
     path('covers/', views.CoverList.as_view(), name='cover-index'),
--- main_app/views.py
+++ main_app/views.py
@@ -5,40 +5,53 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Book, Cover
 from .forms import ReviewForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 
-class BookCreate(CreateView):
+class BookCreate(LoginRequiredMixin, CreateView):
   model = Book
   fields = ['title', 'author', 'description', 'year']
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
   success_url = '/books/'
 
-class BookUpdate(UpdateView):
+class BookUpdate(LoginRequiredMixin, UpdateView):
   model = Book
   fields = ['author', 'description', 'year']
 
-class BookDelete(DeleteView):
+class BookDelete(LoginRequiredMixin, DeleteView):
   model = Book
   success_url = '/books/'
 
-class CoverCreate(CreateView):
+class CoverCreate(LoginRequiredMixin, CreateView):
   model = Cover
   fields = '__all__'
 
-class CoverList(ListView):
+class CoverList(LoginRequiredMixin, ListView):
   model = Cover
 
-class CoverDetail(DetailView):
+class CoverDetail(LoginRequiredMixin, DetailView):
   model = Cover
 
-class CoverUpdate(UpdateView):
+class CoverUpdate(LoginRequiredMixin, UpdateView):
   model = Cover
   fields = ['cover_type', 'color']
 
-class CoverDelete(DeleteView):
+class CoverDelete(LoginRequiredMixin, DeleteView):
   model = Cover
   success_url = '/covers/'
+
+class Home(LoginView):
+  template_name = 'home.html'
 
 def home(request):
   return HttpResponse('<h1>Hellow World</h1>')
@@ -46,10 +59,12 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def book_index(request):
-  books = Book.objects.all()
+  books = Book.objects.filter(user=request.user)
   return render(request, 'books/index.html', { 'books': books })
 
+@login_required
 def book_detail(request, book_id):
   book = Book.objects.get(id=book_id)
   covers_book_dont_have = Cover.objects.exclude(id__in = book.covers.all().values_list('id'))
@@ -60,6 +75,7 @@ def book_detail(request, book_id):
     'covers': covers_book_dont_have
   })
 
+@login_required
 def add_review(request, book_id):
   form = ReviewForm(request.POST)
 
@@ -69,6 +85,22 @@ def add_review(request, book_id):
     new_review.save()
   return redirect('book-detail', book_id=book_id)
 
+@login_required
 def assoc_cover(request, book_id, cover_id):
   Book.objects.get(id=book_id).covers.add(cover_id)
   return redirect('book-detail', book_id=book_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('book-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
